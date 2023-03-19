37a68d9bb55aec52c3f4e487ea45f16a4742fee3
anas sadouq <sadouqanas1@gmail.com>
2023-03-19 15:52:12 +0000

    change the header

--- prospecat_client/src/Blog/Header/Header.js
+++ prospecat_client/src/Blog/Header/Header.js
@@ -4,23 +4,37 @@ import './Header.css';
 export default class Header extends Component{
     render(){
         return(
-            <nav className="container row">
-                <div className="col-3">
-                    <Link to='/'>
-                        <img src="./images/logo.png" id="logo"/>
-                    </Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse">
+            <nav className="navbar navbar-expand-lg navbar-light ">
+                <div className="container-fluid">
+                    <img src="./images/logo.png"/>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                </div>
-                <div className="col-6 my-3">
-                <span className="mx-4"><Link to="/landing_page">About</Link></span>
-                    <span className="mx-4"><Link to="/features">Features</Link></span>
-                    <span className="mx-4"><Link to="/pricing_plan">Pricing</Link></span>
-                    <span className="mx-4"><Link to="/">Blog</Link></span>
-                </div>
-                <div className="col-3 my-2">
-                    <button className="btn btn-success rounded-pill text-center" id="button">chrome Extension</button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-5">
+                            <li className="nav-item mx-2">
+                                <a className="nav-link active" aria-current="page" href="#">About</a>
+                            </li>
+                            <li className="nav-item dropdown mx-2">
+                                <a className="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Features
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active mx-2" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active mx-2" href="#">Blog</a>
+                            </li>
+                        </ul>
+                            <button className="d-flex btn btn-success rounded-pill" id="button" type="submit">Chrome Extension</button>
+                    </div>
                 </div>
             </nav>
         )
