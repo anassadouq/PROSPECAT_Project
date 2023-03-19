8eb2b227706a33a9866543a9d4a5810e18dc7619
anas sadouq <sadouqanas1@gmail.com>
2023-03-19 19:04:59 +0000

    add an add and drop buttons

--- prospecat_client/src/LandingPage/PricingPlan/PricingPlan.js
+++ prospecat_client/src/LandingPage/PricingPlan/PricingPlan.js
@@ -4,51 +4,100 @@ import "./PricingPlan.css";
 
 export default function PricingPlan() {
   const [plans, setPlans] = useState([]);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [leadssearch, setLeadssearch] = useState("");
+  const [emailsearch, setEmailsearch] = useState("");
+  const [mlps, setMlps] = useState("");
+  const [importcsv, setImportcsv] = useState("");
+
   const [selectedPlanIndex, setSelectedPlanIndex] = useState(-1);
 
-  useEffect(()=>{
-    Axios.get("http://localhost:3001/users")
-    .then(res => setPlans(res.data))
-  },[])
+  {/*Show Plans*/}
+    useEffect(()=>{
+      Axios.get("http://localhost:3001/plans")
+      .then(res => setPlans(res.data))
+    },[])
+
+  {/*Create Plan*/}
+    const createPlan = () => {
+      if (title && price && leadssearch && emailsearch && mlps && importcsv) {
+        Axios.post("http://localhost:3001/createPlan", {
+          title, price, leadssearch, emailsearch, mlps, importcsv,
+        }).then((res) => {
+          setPlans((prevPlans) => [...prevPlans, res.data]);
+        });
+      }
+    };
+  
+  {/*Delete Plan*/}
+    const deletePlan = (_id) => {
+      Axios.delete(`http://localhost:3001/deletePlan/${_id}`)
+        .then(res => {
+          console.log(res.data);
+          // success response
+          setPlans((prevPlans) =>
+            prevPlans.filter((plan) => plan._id !== _id)
+          );
+        })
+        .catch(err => {
+          console.error(err);
+          // error response
+        });
+    };
 
   return (
     <div>
       <div className="my-5 mx-3" id="pricingplan">
         <h2 className="text-center" style={{ fontWeight: "bold" }}>Pricing Plan</h2>
         <p className="text-secondary text-center" id="text">We offer multiple pricing models that fit every business size.</p>
-        {plans.map((plan, index) => (
-          <div className="row" key={index}>
-            <div className="col-5 mx-5 ">
-              {selectedPlanIndex === index && (
-                <div>
-                  <h5 id="titleplan" className="mx-5">{plan.title}</h5>
-                  <p>Leads Search
-                    <span className="mx-5" id="numberPlan">{plan.leadssearch}</span>
-                  </p>
-                  <p>Email Search
-                    <span className="mx-5" id="numberPlan">{plan.emailsearch}</span>
-                  </p>
-                  <p>Maximum Leads Per Search
-                    <span className="mx-5" id="numberPlan">{plan.mlps}</span>
-                  </p>
-                  <p>Unlimited CSV IMPORT/EXPORT
-                    <span className="mx-5" id="numberPlan">{plan.importcsv}</span>
-                  </p>
-                  <button className="btn rounded-pill" id="sffn">Start For Free NOW</button>
-                </div>
-              )}
+
+        {/* Create plan*/}
+          <center className="my-5">
+            <input type="text" placeholder="The Plan Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <input type="text" placeholder="The Plan Price" value={price} onChange={(e) => setPrice(e.target.value)}/>
+            <input type="text" placeholder="Leads Search" value={leadssearch} onChange={(e) => setLeadssearch(e.target.value)}/>
+            <input type="text" placeholder="Email Search" value={emailsearch} onChange={(e) => setEmailsearch(e.target.value)}/>
+            <input type="text" placeholder="Maximum Leads Per Search" value={mlps} onChange={(e) => setMlps(e.target.value)}/>
+            <input type="text" placeholder="Unlimited CSV IMPORT/EXPORT" value={importcsv} onChange={(e) => setImportcsv(e.target.value)}/>
+            <button className="btn btn-primary" onClick={createPlan}>Create New Plan</button>
+          </center>
+
+        {/* Show the Plans*/}
+          {plans.map((plan, index) => (
+            <div className="row" key={index}>
+              <div className="col-5 mx-5 ">
+                {selectedPlanIndex === index && (
+                  <div>
+                    <h5 id="titleplan" className="mx-5">{plan.title}</h5>
+                    <p>Leads Search
+                      <span className="mx-5" id="numberPlan">{plan.leadssearch}</span>
+                    </p>
+                    <p>Email Search
+                      <span className="mx-5" id="numberPlan">{plan.emailsearch}</span>
+                    </p>
+                    <p>Maximum Leads Per Search
+                      <span className="mx-5" id="numberPlan">{plan.mlps}</span>
+                    </p>
+                    <p>Unlimited CSV IMPORT/EXPORT
+                      <span className="mx-5" id="numberPlan">{plan.importcsv}</span>
+                    </p>
+                    <button className="btn btn-danger rounded-pill" onClick={() => deletePlan(plan._id)}>Delete Plan</button>
+                    <button className="mx-2 btn rounded-pill" id="sffn">Start For Free NOW</button>
+                  </div>
+                )}
+              </div>
+              <div className="col-4">
+                <ul type="none">
+                  <li>
+                    <button className="btn btn-light button" id="listPlan" onClick={() => setSelectedPlanIndex(index)}>{plan.title}
+                      <span className="mx-5" id="numberPlan">{plan.price}$</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="col-4">
-              <ul type="none">
-                <li>
-                  <button className="btn btn-light button" id="listPlan" onClick={() => setSelectedPlanIndex(index)}>{plan.title}
-                    <span className="mx-5" id="numberPlan">{plan.price}$</span>
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   )
--- prospecat_server/server.js
+++ prospecat_server/server.js
@@ -1,28 +1,59 @@
 const express = require("express")
 const app = express()
-
-const cors = require("cors")
+const cors = require('cors')
 app.use(cors())
+app.use(express.json())
 
 const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://prospecat:prospecat@cluster0.h1vp0lq.mongodb.net/prospecat?retryWrites=true&w=majority")
 
 
-// User model
+// Plan model
 const PlansModel = require('./models/Plans')
 
-// Get request
-app.get("/users", async(req,res)=>{
-    const users = await PlansModel.find();
-    res.json(users)
+// Show Plans
+app.get("/plans", async(req,res)=>{
+    const plans = await PlansModel.find();
+    res.json(plans)
 })
 
-/*app.post("/createUser", async (req, res) => {
+// Create Plan
+app.post("/createPlan", async (req, res) => {
     const newPlan = new PlansModel(req.body);
     await newPlan.save();
     res.json(req.body)
-})*/
+})
 
+
+// Delete Plan
+app.delete("/deletePlan/:id", async (req, res) => {
+    try {
+        const planId = req.params.id;
+        const result = await PlansModel.deleteOne({ _id: planId });
+        res.json({ message: "Plan deleted successfully", result: result });
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting plan", error: error });
+    }
+  });
+
+
+// Update Plan
+app.put('/updatePlan/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, price, leadssearch, emailsearch, mlps, importcsv } = req.body;
+    try {
+      const updatedPlan = await PlansModel.findByIdAndUpdate(
+        id,
+        { title, price, leadssearch, emailsearch, mlps, importcsv },
+        { new: true }
+      );
+      res.json(updatedPlan);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error updating plan');
+    }
+  });
+  
 
 
 app.listen("3001", ()=>{
