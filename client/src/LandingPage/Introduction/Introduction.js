7652e918a74bd5ebbcc54e294d8b5faa5ac1ae5a
anas sadouq <sadouqanas1@gmail.com>
2023-08-06 22:19:30 +0100

    fixe an error

--- client/src/LandingPage/Introduction/Introduction.js
+++ client/src/LandingPage/Introduction/Introduction.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import './Introduction.css';
 export default class Introduction extends Component{
     render(){
@@ -8,10 +7,8 @@ export default class Introduction extends Component{
                 <div className="col-7">
                     <h1 className="my-5" id="prg">It Has Never Been Easier To <br/> Find Leads , Email Adresses...</h1>
                     <p className="text-secondary" id="prg2">Prospecat is a B2B prospecting platform. Use it to <br/>find email addresses and build a quality B2B Data <br/>for your sales funnel.</p>
-                        <Link to="/features">
-                            <button className="btn rounded-pill my-2" id="btn_contact">Get started Now For Free</button>
-                        </Link>
-                        <button className="mx-3 btn rounded-pill" id="btn_contact1">Get Chrome Extension</button>
+                    <button className="btn rounded-pill my-2" id="btn_contact">Get started Now For Free</button>
+                    <button className="mx-3 btn rounded-pill" id="btn_contact1">Get Chrome Extension</button>
                 </div>
                 <div className="col-4">
                     <img src="./images/introduction.png" style={{"height":"450px","width":"400px"}} id="img"/>
